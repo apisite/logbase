c98f61fe2778ae5d4d2c07ae3e2729f42df5a94b
Alexey Kovrizhkin <lekovr@gmail.com>
2025-04-30 02:33:47 +0300

    upd: actual log_format

--- sql/99_data.sql
+++ sql/99_data.sql
@@ -1,9 +1,15 @@
+
 INSERT INTO logs.config(id, key, type_id, data) VALUES
   (1, md5(random()::text), 1, jsonb_build_object(
   'channels', 4
     , 'skip',   '\.(js|gif|png|css|ico|jpg|eot)$'
-    , 'format', '$remote_addr $user1 $user2 [$time_local] "$request" "$status" $size "$referer" "$user_agent" "$t_size" $fresp $fload $end $request_size $request_id'
-    , 'fields', json_build_array('stamp_id', 'file_id', 'line_num', 'time_local', 'remote_addr', 'url', 'args', 'ref_url', 'ref_args', 'referer', 'user_agent', 'method', 'status', 'size', 'fresp', 'fload', 'request_size', 'request_id')
+    , 'format', '$remote_addr - $remote_user [$time_local] "$request" '
+            ||    '$status $size "$referer" '
+            ||    '"$user_agent" "$forwarded_for" '
+            ||    'rt=$request_time uct="$fload" uht="$upstream_header_time" urt="$fresp"'
+-- log_format: ||    'rt=$request_time uct="$upstream_connect_time" uht="$upstream_header_time" urt="$upstream_response_time"'
+    , 'fields', json_build_array('stamp_id', 'file_id', 'line_num', 'time_local', 'remote_addr', 'url', 'args', 'ref_url'
+               , 'ref_args', 'referer', 'user_agent', 'method', 'status', 'size', 'fresp', 'fload')
   ))
  ;
 
